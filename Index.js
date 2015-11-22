c188338f3e58960d131ce57ae4a8e40c83d8e4a9
velkan14 <velkan14@gmail.com>
2015-11-22 15:20:47 +0000

    Scatterplot beggining

--- Index.js
+++ Index.js
@@ -29,6 +29,7 @@ d3.tsv("SITC4_english_structure.tsv", function (data){
 
 d3.tsv("year_origin_sitc2_final.tsv", function (data) {
     dataset = data;
+
     data_sitcut = d3.nest()
     .key(function(d){return d.origin;})
     .key(function(d){return d.sitc.substring(0, 1);})
@@ -61,14 +62,23 @@ d3.tsv("year_origin_sitc2_final.tsv", function (data) {
         );
     }
     data_sitcut = arr;
-
+    dataset.forEach(function(d) {
+        d.year = +d.year;
+        d.import_val = +d.import_val;
+        d.export_val = +d.export_val;
+        d["sitc"] = +d["sitc"];
+    });
     gen_vis();
-    gen_tree();
+    //gen_tree();
+    genScatterPlot();
 })
 
 //onmouseover
 //ter espaço para os idiomas
 //bastam dois idiomas
+function getNameFromCode(code){
+    for(var i = 0; i < data_names.length; i++) if(data_names[i].id_3char == code) return data_names[i].name;
+}
 
 function gen_vis() {   
 	var w = 800;
@@ -85,7 +95,7 @@ function gen_vis() {
         .attr("value",function(d) {
       return d.origin;})
     .text(function(d) {
-        for(var i = 0; i < data_names.length; i++) if(data_names[i].id_3char == d.origin) return data_names[i].name;
+        return getNameFromCode(d.origin);
     });
 
     var hscale = d3.scale.linear()
@@ -290,4 +300,134 @@ function gen_tree() {
       }
       update(d);
     }
+}
+
+function genScatterPlot(){
+var margin = {top: 20, right: 100, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+/* 
+ * value accessor - returns the value to encode for a given data object.
+ * scale - maps value to a visual display encoding, such as a pixel position.
+ * map function - maps from data value to display value
+ * axis - sets up axis
+ */ 
+
+// setup x 
+var xValue = function(d) { return d.sitc;}, // data -> value
+    xScale = d3.scale.linear().range([0, width]), // value -> display
+    xMap = function(d) { return xScale(xValue(d));}, // data -> display
+    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+// setup y
+var yValue = function(d) { return d.import_val;}, // data -> value
+    yScale = d3.scale.linear().range([height, 0]), // value -> display
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+// setup fill color
+var cValue = function(d) { return d.sitc;},
+    color = d3.scale.category10();
+
+// add the graph canvas to the body of the webpage
+var svg = d3.select("#third_chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// add the tooltip area to the webpage
+var tooltip = d3.select("#third_chart").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+// load data
+  var yeardata = [];
+  dataset.forEach(function(d) {
+        if(d.year == 2013){
+          yeardata.push(d);
+        }
+  });
+  var data = [];
+  yeardata.forEach(function(d) {
+        if(d.sitc < 10){
+          data.push(d);
+        }
+  });
+  // change string (from CSV) into number format
+
+  // don't want dots overlapping axis, so add in buffer to data domain
+  xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+  yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+  // x-axis
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Products");
+
+  // y-axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Imports");
+
+  // draw dots
+  svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", xMap)
+      .attr("cy", yMap)
+      .style("fill", function(d) { return color(cValue(d));}) 
+      .on("mouseover", function(d) {
+          tooltip.transition()
+               .duration(200)
+               .style("opacity", .9);
+          tooltip.html( getNameFromCode(d["origin"]) + "<br/> (" + xValue(d) 
+          + ", " + yValue(d) + ")")
+               .style("left", (d3.event.pageX + 5) + "px")
+               .style("top", (d3.event.pageY - 28) + "px");
+      })
+      .on("mouseout", function(d) {
+          tooltip.transition()
+               .duration(500)
+               .style("opacity", 0);
+      });
+
+  // draw legend
+  /*var legend = svg.selectAll(".legend")
+      .data(color.domain())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  // draw legend colored rectangles
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  // draw legend text
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d;});*/
 }
