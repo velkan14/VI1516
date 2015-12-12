9c0f977db3e7b9435e5ac591e62ad1b3f2530bdb
velkan14 <velkan14@gmail.com>
2015-12-12 23:54:59 +0000

    Added files

---
+++ Scatterplot.js
@@ -0,0 +1,175 @@
+function genScatterPlot(){
+  var i = 0;
+  var duration = 700;
+  var margin = {top: 20, right: 100, bottom: 30, left: 40},
+      width = 650 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+      var tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+  // load data
+    var dddd = dataset;
+    var yeardata = [];
+    dataset.forEach(function(d) {
+          if(d.year == 2013){
+            yeardata.push(d);
+          }
+    });
+    var data = [];
+    yeardata.forEach(function(d) {
+          if(d.sitc < width/100){
+            data.push(d);
+          }
+    });
+    // change string (from CSV) into number format
+
+  /*
+   * value accessor - returns the value to encode for a given data object.
+   * scale - maps value to a visual display encoding, such as a pixel position.
+   * map function - maps from data value to display value
+   * axis - sets up axis
+   */
+
+  // setup x
+  var xValue = function(d) { return d.sitc_string;}, // data -> value
+      xScale = d3.scale.ordinal().domain(data.map(function(d){return d.sitc_string;})).rangePoints([20, width-10]), // value -> display CHANGED
+      xMap = function(d) { return xScale(xValue(d));}, // data -> display
+      xAxis = d3.svg.axis().scale(xScale)
+      //.ticks(10)
+      .tickFormat(function (d) {
+          var s = getNameFromSitc(d);
+          if(s.length > 12){
+            return s.substring(0, 12) + "...";
+          }
+          else return s;
+      })
+      .orient("bottom");
+
+  // setup y
+  var yValue = function(d) { return d.import_val;}, // data -> value
+      yScale = d3.scale.linear().range([height, 0]), // value -> display
+      yMap = function(d) { return yScale(yValue(d));}, // data -> display
+      yAxis = d3.svg.axis().scale(yScale)
+      .tickFormat(function (d) {
+          var prefix = d3.formatPrefix(d);
+          return prefix.scale(d);// + prefix.symbol;
+      })
+      .orient("left");
+
+  // setup fill color
+  var cValue = function(d) { return d.sitc;},
+      color = d3.scale.ordinal()
+    .domain(["Food and live animals", "Beverages and tobacco", "Crude materials"])
+    .range(["#3ADF00", "#FE9A2E" , "#8A4B08"]);
+
+  // add the graph canvas to the body of the webpage
+  var svg = d3.select("#rightSide").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // don't want dots overlapping axis, so add in buffer to data domain
+    //xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
+    yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+    // x-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Products");
+
+    // y-axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Imports");
+
+        update(2000,2002);
+        function update(minYear, maxYear){
+          // load data
+            var yeardata = [];
+            dddd.forEach(function(d) {
+                //FIXME: Não sei se deve ser menor ou igual
+                  if(d.year >= minYear && d.year <= maxYear){
+                    yeardata.push(d);
+                  }
+            });
+            var data = [];
+            yeardata.forEach(function(d) {
+              //FIXME: Provavelmente devo receber os nºs de sitc num array
+                  if(d.sitc < width/100){
+                    data.push(d);
+                  }
+            });
+            updateDots(data);
+        }
+
+
+      function updateDots(data){
+        // Update the nodes…
+        var dot = svg.selectAll("g.dot")
+            .data(data, function(d) { return d.id || (d.id = ++i); });
+
+        // Enter any new nodes at the parent's previous position.
+        var dotEnter = dot.enter().append("g")
+            .attr("class", "dot")
+            .attr("transform", function(d) { return "translate(" + xMap(d) + "," + height + ")"; })
+            .on("mouseover", function(d) {
+              d3.select(this.parentNode.appendChild(this)).transition().duration(300)
+              .style({'stroke-opacity':1,'stroke':'#F00'});
+
+                tooltip.transition()
+                     .duration(200)
+                     .style("opacity", .9);
+                tooltip.html( getNameFromCode(d["origin"]) +"<br/>"+ d.year +"<br/>"+ getNameFromSitc(d.sitc_string) + "<br/>" +(yValue(d)/1000000000).toFixed(1)+" billions")
+                     .style("left", (d3.event.pageX + 5) + "px")
+                     .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+              d3.select(this.parentNode.appendChild(this)).transition().duration(300)
+              .style({'stroke-opacity':0,'stroke':'#F00'});
+                tooltip.transition()
+                     .duration(500)
+                     .style("opacity", 0);
+            })
+            ;
+
+        dotEnter.append("circle")
+            .attr("r", 1e-6)
+            .style("fill", function(d) { return color(cValue(d));})
+
+        // Transition nodes to their new position.
+        var dotUpdate = dot.transition()
+            .duration(duration)
+            //.attr("cx", xMap)
+            //.attr("cy", yMap)
+            .attr("transform", function(d) { return "translate(" + xMap(d) + "," + yMap(d) + ")"; });
+
+        dotUpdate.select("circle")
+            .attr("r", 3.5);
+
+        // Transition exiting nodes to the parent's new position.
+        var dotExit = dot.exit().transition()
+            .duration(duration)
+            .attr("transform", function(d) { return "translate(" + xMap(d) + "," + height + ")"; })
+            .remove();
+
+        dotExit.select("circle")
+            .attr("r", 1e-6);
+      }
+}
---
+++ Tree.js
@@ -0,0 +1,160 @@
+function gen_tree() {
+    var w = 600;
+    var h = 600;
+    var i = 0;
+    var duration = 750;
+    var svg = d3.select("#leftSide")
+    .append("svg")
+    .attr("width",w)
+    .attr("height",h);
+
+    // add the tooltip area to the webpage
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    var tree = d3.layout.tree().size([w, h]);
+    var diagonal = d3.svg.diagonal().projection(function projection(d) { return [d.y, d.x];});
+
+    var root = data_sitc;
+    root.x0 = h/2;
+    root.y0 = 0;
+    root.children.forEach(closeAll);
+    update(root);
+
+    function update(source) {
+
+      // Compute the new tree layout.
+      var nodes = tree.nodes(root).reverse(),
+          links = tree.links(nodes);
+
+      // Normalize for fixed-depth.
+      var colors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a'];
+      var j = 0;
+      nodes.forEach(function(d, i) { d.y = d.depth * 180; if(d.depth == 1){ d.color = colors[j++]; }});
+
+      // Update the nodes…
+      var node = svg.selectAll("g.node")
+          .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+      // Enter any new nodes at the parent's previous position.
+      var nodeEnter = node.enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+          .on("click", click)
+          .on("mouseover", function(d){
+              tooltip.transition()
+                 .duration(200)
+                 .style("opacity", .9);
+              tooltip.html(d.value)
+                   .style("left", (d3.event.pageX + 5) + "px")
+                   .style("top", (d3.event.pageY - 28) + "px");})
+          .on("mouseout", function(d) {
+                       tooltip.transition()
+                            .duration(500)
+                            .style("opacity", 0);
+                   })
+          ;
+
+      nodeEnter.append("circle")
+          .attr("r", 1e-6)
+          .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+      nodeEnter.append("text")
+          .attr("x", function(d) { return d.children || d._children ? -13 : 13; })
+          .attr("dy", ".35em")
+          .attr("dx", "2.5em")
+          .attr("text-anchor", "start")
+          .text(function(d) { var s = d.value; if(s.length > 30)return s.substring(0, 30) + "..."; else return s; })
+          .style("fill-opacity", 1e-6);
+
+      // Transition nodes to their new position.
+      var nodeUpdate = node.transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+      nodeUpdate.select("circle")
+          .attr("r", 10)
+          .style("fill", function(d) { if(d.depth == 1) return d.color; else if (d.depth == 2) return d.parent.color; else return "white"; });
+
+      nodeUpdate.select("text")
+          .style("fill-opacity", 1);
+
+      // Transition exiting nodes to the parent's new position.
+      var nodeExit = node.exit().transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+          .remove();
+
+      nodeExit.select("circle")
+          .attr("r", 1e-6);
+
+      nodeExit.select("text")
+          .style("fill-opacity", 1e-6);
+
+      // Update the links…
+      var link = svg.selectAll("path.link")
+          .data(links, function(d) { return d.target.id; });
+
+      // Enter any new links at the parent's previous position.
+      link.enter().insert("path", "g")
+          .attr("class", "link")
+          .attr("d", function(d) {
+            var o = {x: source.x0, y: source.y0};
+            return diagonal({source: o, target: o});
+          });
+
+      // Transition links to their new position.
+      link.transition()
+          .duration(duration)
+          .attr("d", diagonal);
+
+      // Transition exiting nodes to the parent's new position.
+      link.exit().transition()
+          .duration(duration)
+          .attr("d", function(d) {
+            var o = {x: source.x, y: source.y};
+            return diagonal({source: o, target: o});
+          })
+          .remove();
+
+      // Stash the old positions for transition.
+      nodes.forEach(function(d) {
+        d.x0 = d.x;
+        d.y0 = d.y;
+      });
+    }
+
+    // Toggle children on click.
+    function click(d) {
+      if (d.children) {
+        //fechar
+        d._children = d.children;
+        d.children = null;
+      } else {
+        //abrir
+        d.children = d._children;
+        d._children = null;
+      }
+      if(d.parent) d.parent.children.forEach(function(n,i,a)
+      {
+        if(n != d)
+        if (n.children) {
+          //fechar
+          n._children = n.children;
+          n.children = null;
+        }
+      });
+      update(d);
+    }
+
+    function closeAll(d, i, a){
+      if(i != 0)
+      if (d.children) {
+        //fechar
+        d._children = d.children;
+        d.children = null;
+      }
+      //update(d);
+    }
+}
